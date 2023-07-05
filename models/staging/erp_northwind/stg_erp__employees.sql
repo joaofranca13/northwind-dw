1af07b32b28965815a624493005d4eccdbc35783
João França <joao.francapf@gmail.com>
2023-07-05 01:04:12 +0000

    fixed stg_erp__employees

--- models/staging/erp_northwind/stg_erp__employees.sql
+++ models/staging/erp_northwind/stg_erp__employees.sql
@@ -1,36 +1,30 @@
 with 
 
 source as (
-
     select * from {{ source('erp', 'employees') }}
-
 ),
 
-source_employees as (
-
+renamed as (
     select
-        cast(employee_id as int),
-        cast(last_name as string),
-        cast(first_name as string),
-        cast(first_name | as | " " || last_name as string) as full_name,
+        cast(employee_id as int) as employee_id,
+        cast(last_name as string) as last_name,
+        cast(first_name as string) as first_name,
+        cast(first_name || " " || last_name as string) as full_name,
         -- title,
         -- title_of_courtesy,
-        cast(birth_date as date),
-        cast(hire_date as date),
-        cast(address as string),
-        cast(city as string),
-        cast(region as string),
-        cast(postal_code as int),
-        cast(country as string),
+        cast(birth_date as date) as birth_date,
+        cast(hire_date as date) as hire_date,
+        cast(address as string) as address,
+        cast(city as string) as city,
+        cast(region as string) as region,
+        cast(postal_code as int) as postal_code,
+        cast(country as string) as country,
         -- home_phone,
         -- extension,
         -- photo,
-        cast(notes as string),
-        cast(reports_to as int),
+        cast(notes as string) as notes,
+        cast(reports_to as int) as reports_to
         -- photo_path
-
     from source
-
 )
-
-select * from source_employees
+select * from renamed

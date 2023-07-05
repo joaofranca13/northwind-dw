765392ad4a5373b23b474058caf499591ae65df6
João França <joao.francapf@gmail.com>
2023-07-05 01:31:27 +0000

    created employee dimension

---
+++ models/marts/dim_employees.sql
@@ -0,0 +1,30 @@
+with
+
+    erp_employees as (select * from {{ ref("stg_erp__employees") }}),
+
+    erp_managers as (select * from {{ ref("stg_erp__employees") }}),
+
+    joined as (
+        select
+
+            erp_employees.employee_id,
+            erp_employees.last_name,
+            erp_employees.first_name,
+            erp_employees.full_name,
+            erp_managers.full_name as manager_full_name,
+            erp_employees.birth_date,
+            erp_employees.hire_date,
+            erp_employees.address,
+            erp_employees.city,
+            erp_employees.region,
+            erp_employees.postal_code,
+            erp_employees.country,
+            erp_employees.notes,
+            erp_employees.reports_to,
+
+        from erp_employees
+        left join erp_managers on erp_employees.reports_to = erp_managers.employee_id
+    )
+
+select *
+from joined

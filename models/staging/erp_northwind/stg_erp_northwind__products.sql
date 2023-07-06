fa66d540b156d526c7bda4b48676fc2f7d8a3dff
João França <joao.francapf@gmail.com>
2023-07-06 02:53:58 +0000

    create staging table for product dimensions

--- models/staging/erp_northwind/stg_erp_northwind__products.sql
+++ models/staging/erp_northwind/stg_erp_northwind__products.sql
@@ -1,27 +1,20 @@
-with 
+with
 
-source as (
+    source as (select * from {{ source("erp_northwind", "products") }}),
 
-    select * from {{ source('erp_northwind', 'products') }}
-
-),
-
-renamed as (
-
-    select
-        product_id,
-        product_name,
-        supplier_id,
-        category_id,
-        quantity_per_unit,
-        unit_price,
-        units_in_stock,
-        units_on_order,
-        reorder_level,
-        discontinued
-
-    from source
-
-)
-
-select * from renamed
+    renamed as (
+        select
+            cast(product_id as int) as product_id,
+            cast(product_name as string) as product_name,
+            cast(supplier_id as int) as supplier_id,
+            cast(category_id as int) as category_id,
+            cast(quantity_per_unit as string) as quantity_per_unit,
+            cast(unit_price as float) as unit_price,
+            cast(units_in_stock as int) as units_in_stock,
+            cast(units_on_order as int) as units_on_order,
+            cast(reorder_level as int) as reorder_level,
+            case when discontinued = 1 then true else false end as is_discontinued
+        from source
+    )
+select *
+from renamed
